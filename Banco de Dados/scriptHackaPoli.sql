a0f89d0a2027d228107b785d5326ccb00d47e934
Layner <layner2008@hotmail.com>
2016-11-16 17:17:43 -0200

    refatoracao do bd

--- Banco de Dados/scriptHackaPoli.sql
+++ Banco de Dados/scriptHackaPoli.sql
@@ -1,75 +1,121 @@
-CREATE DATABASE `hackapoli` /*!40100 DEFAULT CHARACTER SET utf8 */;
+-- MySQL Workbench Forward Engineering
 
-CREATE TABLE `tbl_pedido` (
-  `codPedido` int(8) NOT NULL AUTO_INCREMENT,
-  `precoTotal` float DEFAULT NULL,
-  `precoDesconto` float DEFAULT NULL,
-  `precoFrete` float DEFAULT NULL,
-  `codUsuario` int(11) DEFAULT NULL,
-  `codTransp` int(11) DEFAULT NULL,
-  `dtaPedido` timestamp NULL DEFAULT NULL,
-  `dataHoraEntrega` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`codPedido`),
-  KEY `codUsuario` (`codUsuario`),
-  KEY `codTransp` (`codTransp`),
-  CONSTRAINT `tbl_pedido_ibfk_1` FOREIGN KEY (`codUsuario`) REFERENCES `tbl_usuario` (`codUsuario`),
-  CONSTRAINT `tbl_pedido_ibfk_2` FOREIGN KEY (`codTransp`) REFERENCES `tbl_transportadora` (`codTransp`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE TABLE `tbl_produto` (
-  `codProduto` int(8) NOT NULL,
-  `nomeProduto` varchar(45) NOT NULL,
-  `preco` float NOT NULL,
-  `peso` float DEFAULT NULL,
-  `tamanho` varchar(45) DEFAULT NULL,
-  `estoque` int(11) DEFAULT NULL,
-  `nomeImg` varchar(45) DEFAULT NULL,
-  `path` varchar(45) DEFAULT NULL,
-  `descricao` varchar(240) NOT NULL,
-  `tags` varchar(100) DEFAULT NULL,
-  `categoria` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`codProduto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- -----------------------------------------------------
+-- Schema hackapoli
+-- -----------------------------------------------------
 
-CREATE TABLE `tbl_produto_pedido` (
-  `codPedido` int(11) DEFAULT NULL,
-  `codProduto` int(11) DEFAULT NULL,
-  KEY `codPedido` (`codPedido`),
-  KEY `codProduto` (`codProduto`),
-  CONSTRAINT `tbl_produto_pedido_ibfk_1` FOREIGN KEY (`codPedido`) REFERENCES `tbl_pedido` (`codPedido`),
-  CONSTRAINT `tbl_produto_pedido_ibfk_2` FOREIGN KEY (`codProduto`) REFERENCES `tbl_produto` (`codProduto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- -----------------------------------------------------
+-- Schema hackapoli
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `hackapoli` DEFAULT CHARACTER SET utf8 ;
+USE `hackapoli` ;
 
-CREATE TABLE `tbl_transportadora` (
-  `codTransp` int(8) NOT NULL AUTO_INCREMENT,
-  `nomeTransp` varchar(45) DEFAULT NULL,
-  `enderecoTransp` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`codTransp`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `tbl_usuario` (
-  `codUsuario` int(8) NOT NULL AUTO_INCREMENT,
-  `nomeCliente` varchar(45) NOT NULL,
-  `contato1` int(9) DEFAULT NULL,
-  `contato2` int(9) DEFAULT NULL,
-  `cgc` int(14) NOT NULL,
-  `cep` int(8) DEFAULT NULL,
-  `tipoLogradouro` varchar(15) DEFAULT NULL,
-  `logradouro` varchar(45) DEFAULT NULL,
-  `numero` int(15) DEFAULT NULL,
-  `cidade` varchar(45) DEFAULT NULL,
-  `email` varchar(45) NOT NULL,
-  `senha` int(8) NOT NULL,
-  `logadouro alternativo` varchar(45) DEFAULT NULL,
-  `tipo logadouro alternativo` varchar(45) DEFAULT NULL,
-  `cep alternativo` varchar(45) DEFAULT NULL,
+-- -----------------------------------------------------
+-- Table `hackapoli`.`tbl_usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `hackapoli`.`tbl_usuario` (
+  `codUsuario` INT(8) NOT NULL AUTO_INCREMENT,
+  `nomeCliente` VARCHAR(45) NOT NULL,
+  `contato1` INT(9) NULL DEFAULT NULL,
+  `contato2` INT(9) NULL DEFAULT NULL,
+  `cgc` INT(14) NOT NULL,
+  `cep` INT(8) NULL DEFAULT NULL,
+  `tipoLogradouro` VARCHAR(15) NULL DEFAULT NULL,
+  `logradouro` VARCHAR(45) NULL DEFAULT NULL,
+  `numero` INT(15) NULL DEFAULT NULL,
+  `cidade` VARCHAR(45) NULL DEFAULT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `senha` INT(8) NOT NULL,
+  `logadouro alternativo` VARCHAR(45) NULL,
+  `tipo logadouro alternativo` VARCHAR(45) NULL,
+  `cep alternativo` VARCHAR(45) NULL,
   PRIMARY KEY (`codUsuario`),
-  UNIQUE KEY `cnpj` (`cgc`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `senha` (`senha`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  UNIQUE INDEX `cnpj` (`cgc` ASC),
+  UNIQUE INDEX `email` (`email` ASC),
+  UNIQUE INDEX `senha` (`senha` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
+-- -----------------------------------------------------
+-- Table `hackapoli`.`tbl_transportadora`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `hackapoli`.`tbl_transportadora` (
+  `codTransp` INT(8) NOT NULL AUTO_INCREMENT,
+  `nomeTransp` VARCHAR(45) NULL DEFAULT NULL,
+  `enderecoTransp` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`codTransp`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
+-- -----------------------------------------------------
+-- Table `hackapoli`.`tbl_pedido`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `hackapoli`.`tbl_pedido` (
+  `codPedido` INT(8) NOT NULL AUTO_INCREMENT,
+  `precoTotal` FLOAT NULL DEFAULT NULL,
+  `precoDesconto` FLOAT NULL DEFAULT NULL,
+  `precoFrete` FLOAT NULL DEFAULT NULL,
+  `codUsuario` INT(8) NULL DEFAULT NULL,
+  `codTransp` INT(8) NULL DEFAULT NULL,
+  `dtaPedido` TIMESTAMP NULL,
+  `dataHoraEntrega` TIMESTAMP NULL,
+  PRIMARY KEY (`codPedido`),
+  INDEX `codUsuario` (`codUsuario` ASC),
+  INDEX `codTransp` (`codTransp` ASC),
+  CONSTRAINT `tbl_pedido_ibfk_1`
+    FOREIGN KEY (`codUsuario`)
+    REFERENCES `hackapoli`.`tbl_usuario` (`codUsuario`),
+  CONSTRAINT `tbl_pedido_ibfk_2`
+    FOREIGN KEY (`codTransp`)
+    REFERENCES `hackapoli`.`tbl_transportadora` (`codTransp`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
+
+-- -----------------------------------------------------
+-- Table `hackapoli`.`tbl_produto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `hackapoli`.`tbl_produto` (
+  `codProduto` INT(8) NOT NULL AUTO_INCREMENT,
+  `nomeProduto` VARCHAR(45) NOT NULL,
+  `preco` FLOAT NOT NULL,
+  `peso` FLOAT NULL DEFAULT NULL,
+  `tamanho` VARCHAR(45) NULL DEFAULT NULL,
+  `estoque` INT(11) NULL DEFAULT NULL,
+  `nomeImg` VARCHAR(45) NULL DEFAULT NULL,
+  `path` VARCHAR(45) NULL DEFAULT NULL,
+  `descricao` VARCHAR(240) NOT NULL,
+  `tags` VARCHAR(100) NULL,
+  `categoria` VARCHAR(45) NULL,
+  PRIMARY KEY (`codProduto`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `hackapoli`.`tbl_produto_pedido`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `hackapoli`.`tbl_produto_pedido` (
+  `codPedido` INT(8) NULL,
+  `codProduto` INT(8) NULL DEFAULT NULL,
+  INDEX `codPedido` (`codPedido` ASC),
+  INDEX `codProduto` (`codProduto` ASC),
+  CONSTRAINT `tbl_produto_pedido_ibfk_1`
+    FOREIGN KEY (`codPedido`)
+    REFERENCES `hackapoli`.`tbl_pedido` (`codPedido`),
+  CONSTRAINT `tbl_produto_pedido_ibfk_2`
+    FOREIGN KEY (`codProduto`)
+    REFERENCES `hackapoli`.`tbl_produto` (`codProduto`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
